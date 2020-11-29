5e5e37bf892df9eb15601d4501374133b44e87e9
Nadeem Muhammad <31960661+mouhanadeem@users.noreply.github.com>
2020-11-29 22:21:27 +0100

    queue

--- src/activeMessageQueue/queue.js
+++ src/activeMessageQueue/queue.js
@@ -1,19 +1,17 @@
 
 export function msgq() {
     let ws = new WebSocket('ws://localhost:61614', 'stomp')
-
-    ws.onopen = () => {
-        console.log('opening...')
-        ws.send('CONNECT\n\n\0')
-        ws.send('subscribe\ndestination:img')
-    }
-
-    ws.onclose = () => console.log('closing...')
-    ws.onmessage = (e) => {
-        console.log(e.data)
-
-        if (e.data.startsWith('MESSAGE')) {
-            console.log(e.data)
-        }
-    }
+ 
+ws.onopen = () => {
+  ws.send('CONNECT\n\n\0')
+ 
+  ws.send('SUBSCRIBE\ndestination:img\n\nack:auto\n\n\0')
+ 
+  ws.send('DISCONNECT\n\n\0')
+}
+ 
+ws.onmessage = (e) => {
+    if (e.data.startsWith(''))
+    console.log(e.data)
+}
 }
